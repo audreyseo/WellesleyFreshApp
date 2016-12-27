521358e62165589b3c5944c3251915d9a9de8932
audreyseo <audreyleeseo@gmail.com>
2016-12-26 19:24:17 -0500

    Added extra helper functions to assist
    I knew I needed to account somehow for when the weeks change because of
    reasons, so naturally, I looked to StackOverflow for some help.
    
    I also fixed the currentHours and totalElapsed functions so they should
    actually do what I want them to do.

--- WellesleyFresh/DayHourRange.swift
+++ WellesleyFresh/DayHourRange.swift
@@ -65,27 +65,45 @@ class DayHourRange:HourRange {
 	override func currentHour() -> Double {
 		let today:Date = Date.init()
 		let todayDate = getDate(aDate: today)
-		let todayHour = getHour(aDate: today)
+		let todayHour = Double(getHour(aDate: today))
 		var totalHours:Double = 0.0
 		
-		var diff:Double = Double(todayDate - lowDay)
-		
-		if diff > 0 {
-			totalHours += (24 - lowHour)
-			diff -= 1.0
-			let diff1 = highDay - todayDate
-			if diff1 == 0 {
-				diff -= 1.0
-				return totalHours + Double(todayHour) + (diff * 24.0)
-			}
-			return totalHours + Double(todayHour)
+		if todayDate != lowDay {
+			totalHours += 24.0 - lowHour
 		} else {
-			return Double(todayHour) - lowHour
+			totalHours += todayHour - lowHour
 		}
+		
+		if todayDate == highDay {
+			totalHours += todayHour
+		}
+		
+		if todayDate > lowDay {
+			totalHours += Double(todayDate - lowDay - 1) * 24.0
+		}
+		
+		return totalHours
+		
+		//var diff:Double = Double(todayDate - lowDay)
+		
+		//if diff > 0 {
+		//	totalHours += (24 - lowHour)
+		//	diff -= 1.0
+		//	let diff1 = highDay - todayDate
+		//	if diff1 == 0 {
+		//		diff -= 1.0
+		//		return totalHours + Double(todayHour) + (diff * 24.0)
+		//	}
+		//	return totalHours + Double(todayHour)
+		//} else {
+		//	return Double(todayHour) - lowHour
+		//}
 	}
 	
 	override func totalChange() -> Double {
-		return (Double(highDay - lowDay) * 24.0) + (24.0 - lowHour) + highHour
+		let dayHours:Double = Double((highDay - lowDay) - 1) * 24.0
+		let hourHours:Double = (24.0 - lowHour) + highHour
+		return dayHours + hourHours
 	}
 	
 	override func elapsedTime() -> Double {
@@ -95,5 +113,51 @@ class DayHourRange:HourRange {
 	
 	func getDate(aDate:Date) -> Int {
 		return Int(dayFormatter.string(from: aDate))!
+	}
+	
+	
+	// I found the following functions and enum on stackoverflow, by Sandeep http://stackoverflow.com/questions/33397101/how-to-get-mondays-date-of-the-current-week-in-swift
+	
+	func getWeekDaysInEnglish() -> [String] {
+		let calendar = NSCalendar(calendarIdentifier: NSCalendar.Identifier.gregorian)!
+		calendar.locale = NSLocale(localeIdentifier: "en_US_POSIX") as Locale
+		return calendar.weekdaySymbols
+	}
+	
+	enum SearchDirection {
+		case Next
+		case Previous
+		
+		var calendarOptions: NSCalendar.Options {
+			switch self {
+			case .Next:
+				return .matchNextTime
+			case .Previous:
+				return [.searchBackwards, .matchNextTime]
+			}
+		}
+	}
+	
+	func get(direction: SearchDirection, _ dayName: String, considerToday consider: Bool = false) -> Date {
+		let weekdaysName = getWeekDaysInEnglish()
+		
+		assert(weekdaysName.contains(dayName), "weekday symbol should be in form \(weekdaysName)")
+		
+		let nextWeekDayIndex = weekdaysName.index(of: dayName)! + 1 // weekday is in form 1 ... 7 where as index is 0 ... 6
+		
+		let today = Date()
+		
+		let calendar = NSCalendar(calendarIdentifier: NSCalendar.Identifier.gregorian)!
+		
+		if consider && calendar.component(.weekday, from: today as Date) == nextWeekDayIndex {
+			return today
+		}
+		
+		let nextDateComponent = NSDateComponents()
+		nextDateComponent.weekday = nextWeekDayIndex
+		
+		
+		let date = calendar.nextDate(after: today as Date, matching: nextDateComponent as DateComponents, options: direction.calendarOptions)
+		return date!
 	}
 }
